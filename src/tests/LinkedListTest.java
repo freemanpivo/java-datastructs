2098394d6bf08e77e068b2891df5cf981f37276a
Pedro Ivo Andrade <andradepedroivo@gmail.com>
2019-02-22 14:54:30 -0300

    Add simple test case

--- src/tests/LinkedListTest.java
+++ src/tests/LinkedListTest.java
@@ -1,5 +1,20 @@
 package tests;
 
-public class LinkedListTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import types.LinkedListImp;
+
+class LinkedListTest {
+
+	@Test
+	void testExample() {
+		LinkedListImp ll = new LinkedListImp();
+		
+		ll.setNumber(1);
+		
+		assertEquals(1, ll.getNumber());
+	}
 
 }
---
+++ src/types/LinkedListImp.java
@@ -0,0 +1,19 @@
+package types;
+
+public class LinkedListImp {
+	private int number;
+
+	public LinkedListImp() {
+		
+	}
+	
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	
+	
+}
