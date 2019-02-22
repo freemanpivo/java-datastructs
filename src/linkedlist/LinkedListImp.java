6a72a4110f8d09f17d350996be96cb2de097db23
Pedro Ivo Andrade <andradepedroivo@gmail.com>
2019-02-22 15:46:18 -0300

    Add insert(), show(), isEmpty(), length() methods

---
+++ src/linkedlist/LinkedListImp.java
@@ -0,0 +1,48 @@
+package linkedlist;
+
+public class LinkedListImp {
+	
+	Node head;
+	
+	public void insert(int data) {
+		Node current = new Node();
+		current.data = data;
+		current.next = null;
+		
+		if (head == null)
+			head = current;
+		else {
+			Node n = head;
+			while (n.next != null) {
+				n = n.next;
+			}
+			n.next = current;
+		}
+	}
+	
+	public void show() {
+		Node node = head;
+		
+		while(node.next != null) {
+			System.out.println(node.data);
+			node = node.next;
+		}
+		// print last element
+		System.out.print(node.data);
+	}
+	
+	public int length() {
+		int length = 0;
+		Node current = head;
+		
+		while(current != null) {
+			length++;
+			current = current.next;
+		}
+		return length;
+	}
+	
+	public boolean isEmpty() {
+		return (length() == 0);
+	}
+}
